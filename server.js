d564b440e2fcd2d39ebd72177f449a3fc2ec4abd
Jon Phoenix <jonathan.martb@gmail.com>
2020-08-30 13:00:26 -0700

    Adding assets

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies - npm packages needed ----------------------------------
+
+// Setting up Express configuration
+
+// Creating the "express" server
+
+// Setting an initial PORT for the Listener
+
+// Setting up the Express app to handle data parsing
+
+
+// API routes ----------------------------------------------------------
+
+// Using a promise to read the JSON
+// Routing the db.json to store / read notes using fs module
+
+// Creating route GET to read the db.json and return saved notes as JSON
+
+// Creating route POST to receive a new note and add it to the db.json
+// Giving each note a unique id when it's saved to the db.json
+// Returning the new note to the client on the front-end
+
+// Writing the notes to the db.json using fs module
+
+// Creating route DELETE to receive a query parameter with the note id
+// Reading saved notes from db.json, removing a note with the given id
+
+// Rewriting the notes to the db.json using fs module
+
+
+// HTML routes ---------------------------------------------------------
+
+// Creating route GET to return the notes.html file - front-end
+
+// Creating route GET to return the index.html file - front-end
+
+
+// Starting the server by setting up the Listener ----------------------
+
+
+// End of program script
